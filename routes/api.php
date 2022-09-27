730aa99959c0ecca2a9be8d52d788c400ef34c00
humbertoyusta <humbertoyusta02@gmail.com>
2022-09-27 20:03:38 +0200

    Uncommenting user middleware

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use App\Http\Controllers\StudentsController;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::get('/students', [StudentsController::class, 'findAll']) -> name('students-find-all');
 
